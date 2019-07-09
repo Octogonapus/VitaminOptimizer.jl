5f352b278dc7be74b96a4e5425dbc62885674775
Ryan Benasutti <firey45@gmail.com>
2019-07-08 23:36:25 -0400

    Store links as tuples

--- src/parseConstraints.jl
+++ src/parseConstraints.jl
@@ -7,32 +7,45 @@ struct DhParam
     alpha::Float64 # Units deg
 end
 
-function parseConstraints!(fileName::String, limbNames::Array{String, 1})
+struct Link
+    dhParam::DhParam
+end
+
+struct Limb
+    maxLinks::Tuple{Vararg{Link}}
+    minLinks::Tuple{Vararg{Link}}
+    tipVelocity::Float64 # Units m/s
+    tipForce::Float64    # Units N
+end
+
+function parseConstraints!(fileName::String, limbNames::Array{String, 1})::Limb
     return parseConstraints(JSON.parsefile(fileName), limbNames)
 end
 
-function parseConstraints(json::Dict{String, Any}, limbNames::Array{String, 1})
+function parseConstraints(json::Dict{String, Any}, limbNames::Array{String, 1})::Limb
     tipVelocity = json["requiredTipVelocityMeterPerSec"]
     tipForce = json["requiredTipForceNewtons"]
     limb = json["HephaestusArmLimbOne"]
-    return parseLimb(limb)
+    (maxConfig, minConfig) = parseLimb(limb)
+    return Limb(
+        maxConfig,
+        minConfig,
+        tipVelocity,
+        tipForce
+    )
 end
 
 function parseLimb(limb::Dict{String, Any})
     minConfig = parseLimbConfig(limb["min"])
     maxConfig = parseLimbConfig(limb["max"])
-
-    return Dict(
-        "max" => maxConfig,
-        "min" => minConfig
-    )
+    return (maxConfig, minConfig)
 end
 
-function parseLimbConfig(config::Dict{String, Any})
+function parseLimbConfig(config::Dict{String, Any})::Tuple{Vararg{Link}}
     links = sort(collect(values(config)), by=x -> x["index"])
-    return map(parseDhParam, links)
+    return tuple(map(x -> Link(parseDhParam(x)), links)...)
 end
 
-function parseDhParam(link::Dict{String, Any})
+function parseDhParam(link::Dict{String, Any})::DhParam
     return DhParam(link["dh-D"] / 1000, link["dh-Theta"], link["dh-A"] / 1000, link["dh-Alpha"])
 end
--- test/parseConstraintsTest.jl
+++ test/parseConstraintsTest.jl
@@ -3,11 +3,31 @@ using Test
 include("../src/parseConstraints.jl")
 
 @testset "parseConstraints" begin
-        @test parseConstraints!("res/testConstraints1.json", ["HephaestusArmLimbOne"]) == Dict(
-                "max" => [DhParam(0.135, 0, 0, -90), DhParam(0, 0, 0.175, 0), DhParam(0, 90, 0.16928, 0)],
-                "min" => [DhParam(0.135, 0, 0, -90), DhParam(0, 0, 0.175, 0), DhParam(0, 90, 0.16928, 0)])
+        @test isequal(
+                parseConstraints!("res/testConstraints1.json", ["HephaestusArmLimbOne"]),
+                Limb(
+                        tuple([Link(DhParam(0.135, 0, 0, -90)),
+                                Link(DhParam(0, 0, 0.175, 0)),
+                                Link(DhParam(0, 90, 0.16928, 0))]...),
+                        tuple([Link(DhParam(0.135, 0, 0, -90)),
+                                Link(DhParam(0, 0, 0.175, 0)),
+                                Link(DhParam(0, 90, 0.16928, 0))]...),
+                        1,
+                        4.90332
+                )
+        )
 
-        @test parseConstraints!("res/testConstraints2.json", ["HephaestusArmLimbOne"]) == Dict(
-                "max" => [DhParam(0.135, 0, 0.1, -90), DhParam(0, 0, 0.2, 0), DhParam(0, 90, 0.2, 0)],
-                "min" => [DhParam(0.135, 0, 0, -90), DhParam(0, 0, 0.175, 0), DhParam(0, 90, 0.16928, 0)])
+        @test isequal(
+                parseConstraints!("res/testConstraints2.json", ["HephaestusArmLimbOne"]),
+                Limb(
+                        tuple([Link(DhParam(0.135, 0, 0.1, -90)),
+                                Link(DhParam(0, 0, 0.2, 0)),
+                                Link(DhParam(0, 90, 0.2, 0))]...),
+                        tuple([Link(DhParam(0.135, 0, 0, -90)),
+                                Link(DhParam(0, 0, 0.175, 0)),
+                                Link(DhParam(0, 90, 0.16928, 0))]...),
+                        0.4,
+                        10
+                )
+        )
 end
