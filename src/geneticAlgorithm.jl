67f65806e2bd241b45ed5a9303caaff0afa9cad5
Ryan Benasutti <firey45@gmail.com>
2019-07-22 14:50:22 -0400

    Add gear ratios

--- src/geneticAlgorithm.jl
+++ src/geneticAlgorithm.jl
@@ -2,7 +2,7 @@ include("parseConstraints.jl")
 include("parseMotorOptions.jl")
 include("featureMatrix.jl")
 
-import LinearAlgebra
+import LinearAlgebra, Distributed
 
 const gravity = 9.80665
 
@@ -27,9 +27,12 @@ struct Entity
 	motor1Index::Int64
 	motor2Index::Int64
 	motor3Index::Int64
-	link1Length::Int64
-	link2Length::Int64
-	link3Length::Int64
+	link1Length::Float64
+	link2Length::Float64
+	link3Length::Float64
+	gearRatio1::Float64
+	gearRatio2::Float64
+	gearRatio3::Float64
 end
 
 """
@@ -37,20 +40,20 @@ Required functions:
 	- GAFitness(entity)
 	- GACrossover(entity1, entity2)
 	- GAMutate(entity, mutationProb)
-	- GAShouldStop(population)
+	- GAShouldStop(population, generationNumber)
 """
 function geneticAlgorithm(initialPopulation::Vector{Entity}, constraints,
-	crossoverProb::Int64, mutationProb::Int64, nElite::Int64, nCross::Int64)::Vector{Entity}
+	mutationProb::Float64, nElite::Int64, nCross::Int64)::Vector{Entity}
 	popNum = length(initialPopulation)
 	nMut = popNum - nElite - nCross
 	population = initialPopulation
 
 	@assert popNum > 0
 	@assert nMut >= 0
-	@assert 0 <= crossoverProb <= 100
-	@assert 0 <= mutationProb <= 100
+	@assert 0 <= mutationProb <= 1
 
-	while !GAShouldStop(population)
+	generationNumber = 0
+	while !GAShouldStop(population, generationNumber)
 		# Step 2
 		fitness = map(GAFitness, population)
 		constraintValues = LinearAlgebra.normalize!(map(
@@ -100,9 +103,10 @@ function geneticAlgorithm(initialPopulation::Vector{Entity}, constraints,
 		elites = sortedPopulation[1:nElite]
 
 		# Step 5
-		crossed = map(_ -> if (rand(1:100) <= crossoverProb) GACrossover(rand(elites), rand(elites)) else rand(elites) end, 1:nCross)
+		crossed = map(_ -> GACrossover(rand(elites), rand(elites)), 1:nCross)
 		mutated = map(_ -> GAMutate(rand(elites), mutationProb), 1:nMut)
 		population = vcat(elites, crossed, mutated)
+		generationNumber += 1
 	end
 
 	return population
@@ -120,52 +124,81 @@ function GAFitness(entity::Entity)::Float64
 end
 
 function GACrossover(entity1::Entity, entity2::Entity)::Entity
-	# Take the motors from entity1 and the links from entity2
+	# Take the motors  and ratios from entity1 and the links from entity2
 	return Entity(entity1.motor1Index, entity1.motor2Index, entity1.motor3Index,
-		entity2.link1Length, entity2.link2Length, entity2.link3Length)
+		entity2.link1Length, entity2.link2Length, entity2.link3Length,
+		entity1.gearRatio1, entity1.gearRatio2, entity1.gearRatio3)
 end
 
-function GAMutate(entity::Entity, mutationProb::Int64)::Entity
+function GAMutate(entity::Entity, mutationProb::Float64)::Entity
 	return Entity(
-		if (rand(1:100) <= mutationProb) rand(1:length(motors)) else entity.motor1Index end,
-		if (rand(1:100) <= mutationProb) rand(1:length(motors)) else entity.motor2Index end,
-		if (rand(1:100) <= mutationProb) rand(1:length(motors)) else entity.motor3Index end,
-		if (rand(1:100) <= mutationProb) rand(limb.minLinks[1].dhParam.r:limb.maxLinks[1].dhParam.r) else entity.link1Length end,
-		if (rand(1:100) <= mutationProb) rand(limb.minLinks[2].dhParam.r:limb.maxLinks[2].dhParam.r) else entity.link2Length end,
-		if (rand(1:100) <= mutationProb) rand(limb.minLinks[3].dhParam.r:limb.maxLinks[3].dhParam.r) else entity.link3Length end
+		if (rand() <= mutationProb) rand(1:length(motors)) else entity.motor1Index end,
+		if (rand() <= mutationProb) rand(1:length(motors)) else entity.motor2Index end,
+		if (rand() <= mutationProb) rand(1:length(motors)) else entity.motor3Index end,
+		if (rand() <= mutationProb) rand(limb.minLinks[1].dhParam.r:limb.maxLinks[1].dhParam.r)/1000 else entity.link1Length end,
+		if (rand() <= mutationProb) rand(limb.minLinks[2].dhParam.r:limb.maxLinks[2].dhParam.r)/1000 else entity.link2Length end,
+		if (rand() <= mutationProb) rand(limb.minLinks[3].dhParam.r:limb.maxLinks[3].dhParam.r)/1000 else entity.link3Length end,
+		if (rand() <= mutationProb) rand(gearRatios) else entity.gearRatio1 end,
+		if (rand() <= mutationProb) rand(gearRatios) else entity.gearRatio2 end,
+		if (rand() <= mutationProb) rand(gearRatios) else entity.gearRatio3 end
 	)
 end
 
-global generationNumber = 0
-function GAShouldStop(population::Vector{Entity})::Bool
-	global generationNumber += 1
-	return generationNumber > 100
+function GAShouldStop(population::Vector{Entity}, generationNumber::Int64)::Bool
+	return generationNumber > 100000
 end
 
 function makeRandomEntity()
-	return GAMutate(Entity(0, 0, 0, 0, 0, 0), 100)
+	# return GAMutate(Entity(0, 0, 0, 0, 0, 0), 1.0)
+	mtrIndex = findfirst(x -> x.name == "stepperMotor-GenericNEMA14", motors)
+	return Entity(mtrIndex, mtrIndex, mtrIndex, 150, 150, 150, 0.05263157894736842, 0.05263157894736842, 0.05263157894736842)
 end
 
 function makeConstraints()
-	return [entity::Entity -> motors[entity.motor1Index].τStall -
-				(limb.tipForce * (entity.link1Length + entity.link2Length + entity.link3Length) +
+	return [entity::Entity -> (limb.tipForce * (entity.link1Length + entity.link2Length + entity.link3Length) +
 				gravity * (motors[entity.motor2Index].mass * entity.link1Length +
-				motors[entity.motor3Index].mass * (entity.link1Length + entity.link2Length))),
-		entity::Entity -> motors[entity.motor2Index].τStall -
-				(limb.tipForce * (entity.link2Length + entity.link3Length) +
-				motors[entity.motor3Index].mass * gravity * entity.link2Length),
-		entity::Entity -> motors[entity.motor3Index].τStall - (limb.tipForce * entity.link3Length),
-		entity::Entity -> motors[entity.motor1Index].ωFree - (limb.tipVelocity / (entity.link1Length + entity.link2Length + entity.link3Length)),
-		entity::Entity -> motors[entity.motor2Index].ωFree - (limb.tipVelocity / (entity.link2Length + entity.link3Length)),
-		entity::Entity -> motors[entity.motor3Index].ωFree - (limb.tipVelocity / entity.link3Length)]
+				motors[entity.motor3Index].mass * (entity.link1Length + entity.link2Length))) -
+				motors[entity.motor1Index].τStall / entity.gearRatio1,
+
+		entity::Entity -> (limb.tipForce * (entity.link2Length + entity.link3Length) +
+				motors[entity.motor3Index].mass * gravity * entity.link2Length) -
+				motors[entity.motor2Index].τStall / entity.gearRatio2,
+
+		entity::Entity -> (limb.tipForce * entity.link3Length) -
+				motors[entity.motor3Index].τStall / entity.gearRatio3,
+
+		entity::Entity -> (limb.tipVelocity / (entity.link1Length + entity.link2Length + entity.link3Length)) -
+				motors[entity.motor1Index].ωFree * entity.gearRatio1,
+
+		entity::Entity -> (limb.tipVelocity / (entity.link2Length + entity.link3Length)) -
+				motors[entity.motor2Index].ωFree * entity.gearRatio2,
+
+		entity::Entity -> (limb.tipVelocity / entity.link3Length) -
+				motors[entity.motor3Index].ωFree * entity.gearRatio3]
 end
 
 global finalPopulation = geneticAlgorithm(
-	map(x -> makeRandomEntity(), 1:10),
+	map(x -> makeRandomEntity(), 1:100),
 	makeConstraints(),
-	40,
+	0.05,
 	5,
-	2,
-	3)
+	90)
 
-println(finalPopulation)
+global bestEntity = finalPopulation[argmax(map(GAFitness, finalPopulation))]
+println("Motor 1=", motors[bestEntity.motor1Index],
+	"\nMotor 2=", motors[bestEntity.motor2Index],
+	"\nMotor 3=", motors[bestEntity.motor3Index],
+	"\nLink 1=", bestEntity.link1Length * 1000,
+	"\nLink 2=", bestEntity.link2Length * 1000,
+	"\nLink 3=", bestEntity.link3Length * 1000,
+	"\nGear ratio 1=", bestEntity.gearRatio1,
+	"\nGear ratio 2=", bestEntity.gearRatio2,
+	"\nGear ratio 3=", bestEntity.gearRatio3)
+
+global bestConstraintValues = map(x -> x(bestEntity), makeConstraints())
+println("Constraint values: ", bestConstraintValues)
+if max(bestConstraintValues...) <= 0
+	println("Feasible.")
+else
+	println("Not feasible.")
+end
