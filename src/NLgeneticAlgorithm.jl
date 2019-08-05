8aea73e87e31f830bbc3b94c3cf8abb062c0500a
Ryan Benasutti <firey45@gmail.com>
2019-08-05 11:24:58 -0400

    Run GA in parallel

--- src/NLgeneticAlgorithm.jl
+++ src/NLgeneticAlgorithm.jl
@@ -5,8 +5,6 @@ include("problemUtil.jl")
 import LinearAlgebra
 using Plots, Statistics, Distributions
 
-const gravity = 9.80665
-
 struct Entity
 	motor1Index::Int64
 	motor2Index::Int64
@@ -17,13 +15,15 @@ struct Entity
 	gearRatio1::Float64
 	gearRatio2::Float64
 	gearRatio3::Float64
+	limb::Limb
+	motors::Array{Motor, 1}
 end
 
 function Base.show(io::IO, entity::Entity)
 	println(io,
-		"Motor 1=", motors[entity.motor1Index],
-		"\nMotor 2=", motors[entity.motor2Index],
-		"\nMotor 3=", motors[entity.motor3Index],
+		"Motor 1=", entity.motors[entity.motor1Index],
+		"\nMotor 2=", entity.motors[entity.motor2Index],
+		"\nMotor 3=", entity.motors[entity.motor3Index],
 		"\nLink 1=", entity.link1Length * 1000,
 		"\nLink 2=", entity.link2Length * 1000,
 		"\nLink 3=", entity.link3Length * 1000,
@@ -126,36 +126,34 @@ function geneticAlgorithm(initialPopulation::Vector{Entity}, constraints,
 	return population, avgFitness
 end
 
-global (limb, motors, gearRatios) = loadProblem(
-	"res/constraints2.json",
-	"HephaestusArmLimbOne",
-	"res/motorOptions.json",
-	1.0)
-
 function GAFitness(entity::Entity)::Float64
 	# Use negative of price because we maximize fitness
-	return -1 * (motors[entity.motor1Index].price + motors[entity.motor2Index].price +
-		motors[entity.motor3Index].price)
+	return -1 * (entity.motors[entity.motor1Index].price + entity.motors[entity.motor2Index].price +
+		entity.motors[entity.motor3Index].price)
 end
 
 function GACrossover(entity1::Entity, entity2::Entity)::Entity
 	# Take the motors  and ratios from entity1 and the links from entity2
-	return Entity(entity1.motor1Index, entity1.motor2Index, entity1.motor3Index,
+	return Entity(
+		entity1.motor1Index, entity1.motor2Index, entity1.motor3Index,
 		entity2.link1Length, entity2.link2Length, entity2.link3Length,
-		entity1.gearRatio1, entity1.gearRatio2, entity1.gearRatio3)
+		entity1.gearRatio1, entity1.gearRatio2, entity1.gearRatio3,
+		entity1.limb, entity1.motors)
 end
 
 function GAMutate(entity::Entity, mutationProb::Float64)::Entity
 	return Entity(
-		if (rand() <= mutationProb) rand(1:length(motors)) else entity.motor1Index end,
-		if (rand() <= mutationProb) rand(1:length(motors)) else entity.motor2Index end,
-		if (rand() <= mutationProb) rand(1:length(motors)) else entity.motor3Index end,
-		if (rand() <= mutationProb) rand(limb.minLinks[1].dhParam.r:limb.maxLinks[1].dhParam.r)/1000 else entity.link1Length end,
-		if (rand() <= mutationProb) rand(limb.minLinks[2].dhParam.r:limb.maxLinks[2].dhParam.r)/1000 else entity.link2Length end,
-		if (rand() <= mutationProb) rand(limb.minLinks[3].dhParam.r:limb.maxLinks[3].dhParam.r)/1000 else entity.link3Length end,
-		if (rand() <= mutationProb) randomGearRatio() else entity.gearRatio1 end,
-		if (rand() <= mutationProb) randomGearRatio() else entity.gearRatio2 end,
-		if (rand() <= mutationProb) randomGearRatio() else entity.gearRatio3 end
+		if (rand() <= mutationProb) rand(1:length(entity.motors)) else entity.motor1Index end,
+		if (rand() <= mutationProb) rand(1:length(entity.motors)) else entity.motor2Index end,
+		if (rand() <= mutationProb) rand(1:length(entity.motors)) else entity.motor3Index end,
+		if (rand() <= mutationProb) rand(entity.limb.minLinks[1].dhParam.r:entity.limb.maxLinks[1].dhParam.r)/1000 else entity.link1Length end,
+		if (rand() <= mutationProb) rand(entity.limb.minLinks[2].dhParam.r:entity.limb.maxLinks[2].dhParam.r)/1000 else entity.link2Length end,
+		if (rand() <= mutationProb) rand(entity.limb.minLinks[3].dhParam.r:entity.limb.maxLinks[3].dhParam.r)/1000 else entity.link3Length end,
+		if (rand() <= mutationProb) randomGearRatio(entity) else entity.gearRatio1 end,
+		if (rand() <= mutationProb) randomGearRatio(entity) else entity.gearRatio2 end,
+		if (rand() <= mutationProb) randomGearRatio(entity) else entity.gearRatio3 end,
+		entity.limb,
+		entity.motors
 	)
 end
 
@@ -164,42 +162,43 @@ Generates a random gear ratio with a 50% chance to gear down and a 50% chance
 to gear up. Simply generating a random number in the allowed range of ratios
 would produce too many ratios greater than 1.
 """
-function randomGearRatio()
+function randomGearRatio(entity::Entity)::Float64
 	if rand() > 0.5
-		return rand(Uniform(1, limb.maxGearRatio))
+		return rand(Uniform(1, entity.limb.maxGearRatio))
 	else
-		return rand(Uniform(1/limb.maxGearRatio, 1))
+		return rand(Uniform(1/entity.limb.maxGearRatio, 1))
 	end
 end
 
 function GAShouldStop(population::Vector{Entity}, generationNumber::Int64)::Bool
-	return generationNumber > maxNumGenerations
+	return generationNumber > 100_000
 end
 
 """
 Each of these constraints is of the form g(x) <= 0.
 """
 function makeConstraints()
-	return [entity::Entity -> (limb.tipForce * (entity.link1Length + entity.link2Length + entity.link3Length) +
-				gravity * (motors[entity.motor2Index].mass * entity.link1Length +
-				motors[entity.motor3Index].mass * (entity.link1Length + entity.link2Length))) -
-				motors[entity.motor1Index].τStall / entity.gearRatio1,
+	gravity::Float64 = 9.80665
+	return [entity::Entity -> (entity.limb.tipForce * (entity.link1Length + entity.link2Length + entity.link3Length) +
+				gravity * (entity.motors[entity.motor2Index].mass * entity.link1Length +
+				entity.motors[entity.motor3Index].mass * (entity.link1Length + entity.link2Length))) -
+				entity.motors[entity.motor1Index].τStall / entity.gearRatio1,
 
-		entity::Entity -> (limb.tipForce * (entity.link2Length + entity.link3Length) +
-				motors[entity.motor3Index].mass * gravity * entity.link2Length) -
-				motors[entity.motor2Index].τStall / entity.gearRatio2,
+		entity::Entity -> (entity.limb.tipForce * (entity.link2Length + entity.link3Length) +
+				entity.motors[entity.motor3Index].mass * gravity * entity.link2Length) -
+				entity.motors[entity.motor2Index].τStall / entity.gearRatio2,
 
-		entity::Entity -> (limb.tipForce * entity.link3Length) -
-				motors[entity.motor3Index].τStall / entity.gearRatio3,
+		entity::Entity -> (entity.limb.tipForce * entity.link3Length) -
+				entity.motors[entity.motor3Index].τStall / entity.gearRatio3,
 
-		entity::Entity -> (limb.tipVelocity / (entity.link1Length + entity.link2Length + entity.link3Length)) -
-				motors[entity.motor1Index].ωFree * entity.gearRatio1,
+		entity::Entity -> (entity.limb.tipVelocity / (entity.link1Length + entity.link2Length + entity.link3Length)) -
+				entity.motors[entity.motor1Index].ωFree * entity.gearRatio1,
 
-		entity::Entity -> (limb.tipVelocity / (entity.link2Length + entity.link3Length)) -
-				motors[entity.motor2Index].ωFree * entity.gearRatio2,
+		entity::Entity -> (entity.limb.tipVelocity / (entity.link2Length + entity.link3Length)) -
+				entity.motors[entity.motor2Index].ωFree * entity.gearRatio2,
 
-		entity::Entity -> (limb.tipVelocity / entity.link3Length) -
-				motors[entity.motor3Index].ωFree * entity.gearRatio3,
+		entity::Entity -> (entity.limb.tipVelocity / entity.link3Length) -
+				entity.motors[entity.motor3Index].ωFree * entity.gearRatio3,
 
 		entity::Entity -> abs(entity.link1Length + entity.link2Length + entity.link3Length - 0.4) - 1e-4,
 
@@ -207,8 +206,8 @@ function makeConstraints()
 end
 
 function makeNLForceConstaint(entity::Entity)::Float64
-	θ3 = π/2 - atan(limb.targetZ/1000, limb.targetX/1000)
-	h = sqrt((limb.targetX/1000)^2 + ((limb.targetZ/1000) - entity.link1Length)^2)
+	θ3 = π/2 - atan(entity.limb.targetZ/1000, entity.limb.targetX/1000)
+	h = sqrt((entity.limb.targetX/1000)^2 + ((entity.limb.targetZ/1000) - entity.link1Length)^2)
 
 	if h > entity.link2Length + entity.link3Length
 		return 1.0
@@ -236,49 +235,77 @@ function makeNLForceConstaint(entity::Entity)::Float64
 	J = [0 (entity.link3Length * cos(α + β) - entity.link2Length * sin(α)) (entity.link3Length * cos(α + β));
 	     (entity.link1Length + entity.link3Length * sin(α + β) + entity.link2Length * cos(α)) 0 0;
 		 0 (-entity.link3Length * sin(α + β) - entity.link2Length * cos(α)) (-entity.link3Length * sin(α + β))]
-	jointTorques = transpose(J) * [0, 0, limb.tipForceClosePos]
+	jointTorques = transpose(J) * [0, 0, entity.limb.tipForceClosePos]
 
-	return (jointTorques[2] - motors[entity.motor2Index].τStall / entity.gearRatio2) +
-	       (jointTorques[3] - motors[entity.motor3Index].τStall / entity.gearRatio3)
+	return (jointTorques[2] - entity.motors[entity.motor2Index].τStall / entity.gearRatio2) +
+	       (jointTorques[3] - entity.motors[entity.motor3Index].τStall / entity.gearRatio3)
 end
 
-function makeRandomEntity()
-	return GAMutate(Entity(0, 0, 0, 0, 0, 0, 0, 0, 0), 1.0)
+function makeRandomEntity(limb::Limb, motors::Array{Motor, 1})::Entity
+	return GAMutate(Entity(0, 0, 0, 0, 0, 0, 0, 0, 0, limb, motors), 1.0)
 end
 
-function isFeasible(entity::Entity)
+function isFeasible(entity::Entity)::Bool
 	constraintValues = map(x -> x(entity), makeConstraints())
 	return maximum(constraintValues) <= 0
 end
 
-const global maxNumGenerations = 10_000
+function runOnce(limb::Limb, motors::Array{Motor, 1})
+	(finalPopulation, avgFitness), totalTime, bytes, gcTime, memAllocs = @timed geneticAlgorithm(
+		map(x -> makeRandomEntity(limb, motors), 1:100),
+		makeConstraints(),
+		0.05,
+		0.25,
+		1 - 0.25 - 0.05)
 
-global (finalPopulation, avgFitness), totalTime, bytes, gcTime, memAllocs = @timed geneticAlgorithm(
-	map(x -> makeRandomEntity(), 1:100),
-	makeConstraints(),
-	0.05,
-	0.25,
-	1 - 0.25 - 0.05)
+	# println("Final population:")
+	# for x::Entity in finalPopulation
+	# 	println(x)
+	# end
 
-println("Final population:")
-for x::Entity in finalPopulation
-	println(x)
+	feasibleEntities = filter(x -> isFeasible(x), finalPopulation)
+
+	bestFitness = 0.0
+	bestEntities = Set{Entity}()
+	if !isempty(feasibleEntities)
+		bestFitness = maximum(map(x -> GAFitness(x), feasibleEntities))
+		bestEntities = Set(filter(x -> GAFitness(x) ≈ bestFitness, feasibleEntities))
+
+		# println("Best entities:")
+		# for x::Entity in bestEntities
+		# 	println(x)
+		# end
+	end
+
+	println("Time: ", totalTime)
+
+	# avgFitnessPerGen = plot(avgFitness, title="Average Fitness per Generation",
+	# 	label=["Average Fitness"], xlabel="Generation Number")
+	# savefig(avgFitnessPerGen, "average_fitness_per_generation.png")
+
+	return bestFitness, bestEntities
 end
 
-global feasibleEntities = filter(x -> isFeasible(x), finalPopulation)
+function runN(numRuns::Int64)
+	global (limb, motors, gearRatios) = loadProblem(
+		"res/constraints2.json",
+		"HephaestusArmLimbOne",
+		"res/motorOptions.json",
+		1.0)
 
-if !isempty(feasibleEntities)
-	global bestFitness = maximum(map(x -> GAFitness(x), feasibleEntities))
-	global bestEntities = Set(filter(x -> GAFitness(x) ≈ bestFitness, feasibleEntities))
+	runs = []
+	Threads.@threads for i = 1:numRuns
+		push!(runs, runOnce(limb, motors))
+	end
 
-	println("Best entities:")
-	for x::Entity in bestEntities
-		println(x)
+	fitness = map(x -> x[1], runs)
+	bestFitnessPerOpt = plot(1:numRuns, fitness, title="Best Fitness per Optimization",
+		label=["Best Fitness"], xlabel="Optimization Number")
+	savefig(bestFitnessPerOpt, "best_fitness_per_optimization.png")
+
+	for x = runs
+		println(x[2])
 	end
 end
 
-println("Time: ", totalTime)
-
-avgFitnessPerGen = plot(1:(maxNumGenerations+1), avgFitness, title="Average Fitness per Generation",
-	label=["Average Fitness"], xlabel="Generation Number")
-savefig(avgFitnessPerGen, "average_fitness_per_generation.png")
+runN(10)
