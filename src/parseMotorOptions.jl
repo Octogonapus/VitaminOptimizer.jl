be7245aee2e758ed87e4d203f1ea77535fbdebfa
Ryan Benasutti <firey45@gmail.com>
2019-08-08 14:32:25 -0400

    Translate old motor options and update parsing

--- src/parseMotorOptions.jl
+++ src/parseMotorOptions.jl
@@ -6,28 +6,15 @@ function parseMotorOptions!(fileName::String)::Array{Motor, 1}
     return parseMotorOptions(JSON.parsefile(fileName))
 end
 
-function parseMotorOptions(json::Dict{String, Any})::Array{Motor, 1}
-    motorClasses = json["motors"]
-    entries = collect(motorClasses)
-    motors = collect(Iterators.flatten(
-        [parseMotorClass(json, className, members) for
-            (className, members::Array{String, 1})=entries]))
-    return motors
+function parseMotorOptions(json::Array{Any, 1})::Array{Motor, 1}
+    return [parseMotor(it) for it=json]
 end
 
-function parseMotorClass(
-    json::Dict{String, Any},
-    className::String,
-    members::Array{String, 1})
-    return [parseMotor(className * "-" * member,
-            json["data"][className * "-" * member]) for member=members]
-end
-
-function parseMotor(name::String, motorData::Dict{String, Any})
+function parseMotor(data::Dict{String, Any})
     return Motor(
-        name,
-        motorData["MaxTorqueNewtonmeters"],
-        motorData["MaxFreeSpeedRadPerSec"],
-        motorData["price"],
-        motorData["massKg"])
+        data["name"],
+        data["τStall"],
+        data["ωFree"],
+        data["price"],
+        data["mass"])
 end
